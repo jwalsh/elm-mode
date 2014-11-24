5a5af3596be269397bfbd26f321fe4a7fd3f2f4a
Jason Walsh <jwalsh@booster.com>
2014-11-23 21:51:07 -0500

    Failig font-locking cases
    - Assignment
    - Test
    - Concat
    - EOL comments
    - Math operators

--- tests/020_examples/name.elm
+++ tests/020_examples/name.elm
@@ -1,9 +1,17 @@
+-- Assignment should be highlighted
 hello name =
-    -- asText is
+    -- asText is DOM
+    -- Tests should be highlighted
     if name == "Jason"
     then "Self"
+    -- Concat should be highlighlighted
     else "Other: " ++ name
 
+
+-- Int should be highlighted
+-- Additional operator should be highlighted
+six = 4 + 2
+-- Line comment should be highlighted
 number = "6" -- 4 + 2
 
 main = asText [ hello "Bob"
