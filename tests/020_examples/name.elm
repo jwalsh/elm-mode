177fe46927ccf247d495da9b767fe8ecb0900855
Jason Walsh <jwalsh@booster.com>
2014-11-23 21:46:50 -0500

    Variable, funcall, and number font-locking
    Reviewing highlighting on elm-lang.org/try

--- tests/020_examples/name.elm
+++ tests/020_examples/name.elm
@@ -1,8 +1,12 @@
 hello name =
+    -- asText is
     if name == "Jason"
     then "Self"
     else "Other: " ++ name
 
+number = "6" -- 4 + 2
+
 main = asText [ hello "Bob"
+              , hello number
               , hello "Jason"
               , hello "Phil"]
